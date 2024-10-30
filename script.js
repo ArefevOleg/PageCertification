097841d3ed381b6d3437ba2846a6e2b632de8f3d
Arefevpro <arefevmsk@gmail.com>
2024-10-30 21:41:08 +0300

    commit 7

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+  const anchors = document.querySelectorAll('nav a');
 
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-    const targetHeading = targetSection.querySelector('.title');
+  anchors.forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
 
-    // Изменение цвета заголовка на секунду
-    targetHeading.style.textShadow= '2px 2px 5px rgb(241, 210, 12)';
-    setTimeout(() => {
-      targetHeading.style.textShadow = '';
-    }, 1500);
+          const targetId = this.getAttribute('href').substring(1);
+          const targetSection = document.getElementById(targetId);
+          const targetHeading = targetSection ? targetSection.querySelector('.title') : null;
 
-    // Плавный скроллинг
-    targetSection.scrollIntoView({
-      behavior: 'smooth'
-    });
+          if (targetHeading) {
+              // Изменение цвета заголовка на секунду
+              targetHeading.style.textShadow = '2px 2px 5px rgb(241, 210, 12)';
+              setTimeout(() => {
+                  targetHeading.style.textShadow = '';
+              }, 1500);
+
+              // Плавный скроллинг
+              targetSection.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+
+          // Проверка, является ли элемент подменю
+          const submenu = this.nextElementSibling;
+          if (submenu && submenu.classList.contains('submenu')) {
+              e.stopPropagation();
+              submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+          }
+      });
   });
 });
